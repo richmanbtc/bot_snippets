681dc3da84e27bc864581c110784d44bdedf266a
richmanbtc <81956477+richmanbtc@users.noreply.github.com>
2021-12-02 16:29:46 +0900

    Add files via upload

---
+++ my_keras_regressor2.py
@@ -0,0 +1,72 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
+import marshal
+import types
+import traceback
+
+# 追加機能
+# mc dropout
+# early stopping (fit_context版)
+
+class MyKerasRegressor2(KerasRegressor):
+    def __init__(self, mc_count=None, fit_params={}, **kwargs):
+        KerasRegressor.__init__(self, **kwargs)
+        self.mc_count_ = mc_count
+        self.fit_params_ = fit_params
+
+    def fit(self, X, y, fit_context=None, **kwargs):
+        if fit_context is None:
+            return KerasRegressor.fit(self, X, y, **self.fit_params_, **kwargs)
+        else:
+            return KerasRegressor.fit(
+                self,
+                X,
+                y,
+                validation_data=(fit_context['X_val'], fit_context['y_val']),
+                **self.fit_params_,
+                **kwargs,
+            )
+
+    def predict(self, X=None):
+        if self.mc_count_ is None or self.mc_count_ == 1:
+            return KerasRegressor.predict(self, X)
+
+        ys = []
+
+        X = tf.data.Dataset.from_tensor_slices(X)
+        X = X.batch(65536)
+
+        for i in range(self.mc_count_):
+            ys.append(KerasRegressor.predict(self, X))
+
+        return np.mean(ys, axis=0)
+
+    def get_params(self, **params):
+        res = KerasRegressor.get_params(self, **params)
+        res.update({
+            'mc_count': self.mc_count_,
+            'fit_params': self.fit_params_,
+        })
+        return res
+
+    def set_params(self, **params):
+        self.mc_count_ = params['mc_count']
+        self.fit_params_ = params['fit_params']
+        params = params.copy()
+        del params['mc_count']
+        del params['fit_params_']
+        return KerasRegressor.set_params(self, **params)
+
+    # https://stackoverflow.com/questions/8574742/how-to-pickle-an-object-of-a-class-b-having-many-variables-that-inherits-from
+    def __getstate__(self):
+        a_state = KerasRegressor.__getstate__(self)
+        b_state = {
+            'mc_count_': self.mc_count_,
+        }
+        return (a_state, b_state)
+
+    def __setstate__(self, state):
+        a_state, b_state = state
+        self.mc_count_ = b_state['mc_count_']
+        KerasRegressor.__setstate__(self, a_state)
